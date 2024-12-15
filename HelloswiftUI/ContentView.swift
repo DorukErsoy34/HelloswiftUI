a4a927d77aed1fd105e609696d9a7d6ffd49b251
DorukErsoy34 <doruk.ersoy@hisarschool.k12.tr>
2024-12-15 11:43:36 +0300

    Initial Commit

---
+++ HelloswiftUI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  HelloswiftUI
+//
+//  Created by Doruk ERSOY on 12/14/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Knock, knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloswiftUI/HelloswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloswiftUIApp.swift
+//  HelloswiftUI
+//
+//  Created by Doruk ERSOY on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
